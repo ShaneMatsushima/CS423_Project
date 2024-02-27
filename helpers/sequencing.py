85e350f66425ddba6f5101062e08f643a3970d8e
ShaneMatsushima <shanematsushima@gmail.com>
2024-02-27 14:11:01 -0800

    implemented dict of seq

--- helpers/sequencing.py
+++ helpers/sequencing.py
@@ -7,8 +7,13 @@ This is to ensure seperation from gui and functionality for easier testing.
 Created: 2/12/2024
 Author: Shane Matssuhima, Ian Thompson, Austen Furutani
 '''
+import json
 
 #TODO write sequencing functions for raw data -> output: information of sequence
+
+def loadDict(dictPath:str) -> dict:
+    with open(dictPath, 'r') as file:
+        return json.load(file)
 
 if __name__ == '__main__':
     ...
