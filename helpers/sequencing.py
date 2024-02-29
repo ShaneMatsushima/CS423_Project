02e98ffa7e79a36c192c70f8b51491b96ff146be
ShaneMatsushima <shanematsushima@gmail.com>
2024-02-28 20:08:29 -0800

    implement predicting with seq text

--- helpers/sequencing.py
+++ helpers/sequencing.py
@@ -8,23 +8,38 @@ Created: 2/12/2024
 Author: Shane Matssuhima, Ian Thompson, Austen Furutani
 '''
 import json
+import os
 #TODO write sequencing functions for raw data -> output: information of sequence
 
 def loadDict(dictPath:str) -> dict:
     with open(dictPath, 'r') as file:
         return json.load(file)
 
-def seq_to_fasta(seq_data:dict, gi:str)->str:
-
-    output_file = "helpers\tmp.fasta" # tmp for running alphafold since it likes fasta files
+def seqToFasta(seq:str)->str:
+    
+    output_file = "helpers\\tmp.fasta" # tmp for running alphafold since it likes fasta files
+    print(os.path.join(os.getcwd(), output_file))
 
     with open(output_file, 'w') as file: # clear tmp file so fasta not appended
         file.write("")
 
     with open(output_file, 'w') as file:
-        file.write(f'>{gi}\n{seq_data[gi]}\n')
+        file.write(f'>tmp fasta\n{seq}\n')
     
     return output_file
+
+def checkProteinSeq(seq:str) ->bool:
+    seq = seq.upper()
+    if  len(seq) % 3 != 0:
+        return False
+    if not seq:
+        return False
+    
+    for i in seq:
+        if i not in "ABCDEFGHIKLMNPQRSTVWY":
+            return False
+    
+    return True
 
 if __name__ == '__main__':
     # put test code here
